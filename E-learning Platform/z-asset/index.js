6ca5025410b9296a2a4be7f78ab31b85034c5043
Kuraloviya K <107206093+Kural23@users.noreply.github.com>
2024-04-03 11:06:14 +0530

    Add files via upload

---
+++ E-learning Platform/z-asset/index.js
@@ -0,0 +1,22 @@
+function animateValue(id, start, end, duration) {
+    if (start === end) return;
+    var range = end - start;
+    var current = start;
+    var increment = end > start? 1 : -1;
+    var stepTime = Math.abs(Math.floor(duration / range));
+    var obj = document.getElementById(id);
+    var timer = setInterval(function() {
+        current += increment;
+        obj.innerHTML = current;
+        if (current == end) {
+            clearInterval(timer);
+        }
+    }, stepTime);
+}
+
+animateValue("value", 1339, 1347, 800);
+animateValue("cour", 58, 67, 800);
+animateValue("even", 36, 24, 800);
+animateValue("train", 34, 42, 800);
+
+
